c380e3caca10ae332417d3addcdff429f90722b9
ccolonia <60549081+ccolonia@users.noreply.github.com>
2025-09-29 18:13:57 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'imc-app-v1';
+const urlsToCache = [
+  '/',
+  '/imc-pwa/',
+  '/imc-pwa/index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
